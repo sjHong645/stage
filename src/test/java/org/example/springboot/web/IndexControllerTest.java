213e846dd5e603d8e99770afee201fffd8e6bd7e
sponbob-pat <ixdos1111@gmail.com>
2022-08-08 17:26:04 +0900

    동시 검색 기능 추가

--- src/test/java/org/example/springboot/web/IndexControllerTest.java
+++ src/test/java/org/example/springboot/web/IndexControllerTest.java
@@ -21,10 +21,28 @@ public class IndexControllerTest {
     public void mainPage_loaded() {
 
         // when
+        // TestRestTemplate을 통해 "/"로 호출했을 때
+        // index.mustache에 포함된 코드들이 있는지 확인
         String body = this.restTemplate.getForObject("/", String.class);
 
         // then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        // assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("채용 공고 동시 검색");
     }
+
+    // 검색을 위한 테스트를 꼭 만들자
+    /*@Test
+    public void search_completed() {
+
+        // when
+        // TestRestTemplate을 통해 "/"로 호출했을 때
+        // index.mustache에 포함된 코드들이 있는지 확인
+        String body = this.restTemplate.getForObject("/", String.class);
+
+
+        assertThat(body).contains("검색");
+
+
+    }*/
 
 }
