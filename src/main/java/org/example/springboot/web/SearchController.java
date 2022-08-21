d3fafadc31d0522ac5283f60dbb0ceb2cfe138e4
sponbob-pat <ixdos1111@gmail.com>
2022-08-21 22:10:23 +0900

    사람인에서 모든 채용정보 페이지를 크롤링할 수 있게함 페이지 번호를 하나씩 넘겨가면서 결과를 저장함

--- src/main/java/org/example/springboot/web/SearchController.java
+++ src/main/java/org/example/springboot/web/SearchController.java
@@ -1,25 +1,13 @@
 package org.example.springboot.web;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.RestController;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.example.springboot.web.recruiteWebSite.SaramIn;
 
 @Controller
 public class SearchController {
@@ -48,8 +36,6 @@ public class SearchController {
         // {{name}}에 attributeValue를 출력한다.
         model.addAttribute("name", "홍승준");
 
-        String results = "";
-
         // 여기서 results에 다가 결과값을 저장하면 화면에 출력 가능
 
         // 파라미터로 company를 전달했음
@@ -62,75 +48,9 @@ public class SearchController {
         model.addAttribute("jobKorea", jobKorea);
         model.addAttribute("wanted", wanted);
 
-        model.addAttribute("results", results);
-
-        model.addAttribute("saramInList", saramInSearch(saramIn, company));
-        // model.addAttribute("jobKoreaList", saramInSearch(saramIn));
-        // model.addAttribute("wantedList", saramInSearch(saramIn));
+        model.addAttribute("saramInList", SaramIn.saramInSearch(saramIn));
 
         return "multiSearch";
-    }
-
-    // 파라미터 str = 검색 url
-    // 일단, 채용정보 1페이지에 있는 내용만 출력하도록 하자.
-    // 채용정보 파라미터는 recruitPage. company 뒤에 &로 이어서 전달하면 된다.
-    // ex) 2페이지 - recruitPage = 2
-    public String saramInSearch(String str, String company) throws IOException {
-
-        String result = "";
-
-        WebDriver driver;
-
-        final String WEB_DRIVER_ID = "webdriver.chrome.driver";
-        final String WEB_DRIVER_PATH = "C:/devtool/chromedriver_win32/chromedriver.exe"; // 크롬 드라이버 경로
-
-        String base_url; // 크롤링 할 url
-
-        // System Property 설정
-        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
-
-        driver = new ChromeDriver();
-        base_url = str;
-
-        try {
-
-            //get page (= 브라우저에서 url을 주소창에 넣은 후 request 한 것과 같다)
-            driver.get(base_url);
-
-            // item_recruit라는 이름의 클래스 리스트를 불러옴
-            // List<WebElement> el1 = driver.findElements(By.className("job_tit"));
-
-            // <div id = "recruit_info_list"> -> <div class = "content">
-            // -> <div class = "item_recruit"> -> <div class = "area_job"> -> <h2 class = "job_tit">
-            List<WebElement> titleList = driver.findElements(
-                    By.cssSelector("#recruit_info_list > div.content > div.item_recruit > div.area_job > h2.job_tit"));
-
-            List<WebElement> companyList = driver.findElements(
-                    By.cssSelector("#recruit_info_list > div.content > div.item_recruit > div.area_corp > strong.corp_name"));
-
-            System.out.println("titleList.size() = " + titleList.size());
-
-            for(int i = 0; i < titleList.size(); i++) {
-
-                // System.out.println(el1.get(i).getText());
-                // {}를 3개 쓰면서 <br>을 추가해줌으로써 줄 바꿈이 가능해짐
-                result += titleList.get(i).getText()
-                        + " "
-                        + companyList.get(i).getText() + "<br>";
-
-            }
-
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-        } finally {
-            driver.close();
-        }
-
-
-        return result;
     }
 
 
---
+++ src/main/java/org/example/springboot/web/recruiteWebSite/SaramIn.java
@@ -0,0 +1,87 @@
+package org.example.springboot.web.recruiteWebSite;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.LinkedList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class SaramIn {
+
+    // 파라미터 str = 검색 url
+    // 일단, 채용정보 1페이지에 있는 내용만 출력하도록 하자.
+    // 채용정보 파라미터는 recruitPage. company 뒤에 &로 이어서 전달하면 된다.
+    // ex) 2페이지 - recruitPage = 2
+    public static String saramInSearch(String str) throws IOException {
+
+        String result = "";
+
+        WebDriver driver;
+
+        final String WEB_DRIVER_ID = "webdriver.chrome.driver";
+        final String WEB_DRIVER_PATH = "C:/devtool/chromedriver_win32/chromedriver.exe"; // 크롬 드라이버 경로
+
+        String base_url; // 크롤링 할 url
+
+        // System Property 설정
+        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+
+        driver = new ChromeDriver();
+        base_url = str + "&recruitPage="; // 페이지 번호 매개변수를 추가한 url
+
+        List<WebElement> titleList = new LinkedList<WebElement>();
+        List<WebElement> companyList = new LinkedList<WebElement>();
+
+        try {
+            int pageNumber = 1;
+
+            int prevCount = 0;
+            while(true) {
+                // get page (= 브라우저에서 url을 주소창에 넣은 후 request 한 것과 같다)
+                // 반복문이 돌아올 때 마자 페이지 번호를 1씩 증가시킨다.
+                driver.get(base_url + Integer.toString(pageNumber++));
+
+                // item_recruit라는 이름의 클래스 리스트를 불러옴
+
+                // <div id = "recruit_info_list"> -> <div class = "content">
+                // -> <div class = "item_recruit"> -> <div class = "area_job"> -> <h2 class = "job_tit">
+                titleList = driver.findElements(
+                        By.cssSelector("#recruit_info_list > div.content > div.item_recruit > div.area_job > h2.job_tit"));
+
+                companyList = driver.findElements(
+                        By.cssSelector("#recruit_info_list > div.content > div.item_recruit > div.area_corp > strong.corp_name"));
+
+                // 더 이상 검색결과가 없을 때까지 반복한다.
+                if(titleList.size() == 0) break;
+
+                try {
+                    for(int i = 0; i < titleList.size(); i++) {
+
+                        // {}를 3개 쓰면서 <br>을 추가해줌으로써 줄 바꿈이 가능해짐
+                        result += "채용 공고 = " + titleList.get(i).getText()
+                                + " "
+                                + " 회사 = " + companyList.get(i).getText() + "<br>";
+
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+        driver.close();
+
+        return result;
+    }
+}
